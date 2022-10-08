8f62ae7baf0b2d2a7b9eed486102445943d2b40e
PremaKathiresanVasagam <premavasagam@gmail.com>
2022-10-08 00:52:29 +0000

    s4 gradio updated code

---
+++ src/cifar10_demo_scripted.py
@@ -0,0 +1,77 @@
+import pyrootutils
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+from typing import List, Tuple
+
+import urllib
+
+import numpy as np
+from PIL import Image
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+import torch
+import hydra
+import gradio as gr
+from omegaconf import DictConfig
+
+from src import utils
+
+log = utils.get_pylogger(__name__)
+
+def demo(cfg: DictConfig) -> Tuple[dict, dict]:
+    """Demo function.
+    Args:
+        cfg (DictConfig): Configuration composed by Hydra.
+
+    Returns:
+        Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
+    """
+
+    assert cfg.ckpt_path
+
+    log.info("Running Demo")
+
+    log.info(f"Instantiating scripted model <{cfg.ckpt_path}>")
+    model = torch.jit.load(cfg.ckpt_path)
+
+    log.info(f"Loaded Model: {model}")
+
+    classes=['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+
+    def predict_cifar10_images(image):
+        if image is None:
+            return None
+        config = resolve_data_config({}, model=model)
+        transform = create_transform(**config)
+
+        img_tensor = transform(image).unsqueeze(0)  # transform and add batch dimension
+        
+        preds = model.forward_jit(img_tensor)
+
+        preds = preds[0].tolist()
+        label_pred = {classes[i]: preds[i] for i in range(10)}
+
+        return label_pred
+
+    demo = gr.Interface(
+        fn=predict_cifar10_images,
+        inputs=gr.Image(type='pil'),
+        outputs=gr.Label(num_top_classes=10),
+    )
+    demo.launch(share=True)
+
+@hydra.main(
+    version_base="1.2", config_path=root / "configs", config_name="cifar10_demo_scripted.yaml"
+)
+def main(cfg: DictConfig) -> None:
+    demo(cfg)
+
+if __name__ == "__main__":
+    main()
--- src/models/timm_module.py
+++ src/models/timm_module.py
@@ -4,7 +4,8 @@ import torch
 from pytorch_lightning import LightningModule
 from torchmetrics import MaxMetric
 from torchmetrics.classification.accuracy import Accuracy
-
+import torch.nn.functional as F
+from torchvision import transforms as T
 
 class TIMMLitModule(LightningModule):
     """Example of LightningModule for CIFAR10 classification.
@@ -46,8 +47,23 @@ class TIMMLitModule(LightningModule):
         # for logging best so far validation accuracy
         self.val_acc_best = MaxMetric()
 
+        self.predict_transform = T.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+
     def forward(self, x: torch.Tensor):
         return self.net(x)
+
+    @torch.jit.export
+    def forward_jit(self, x: torch.Tensor):
+
+        with torch.no_grad():
+            # transform the inputs
+            x = self.predict_transform(x)
+
+            #forward pass
+            logits = self(x)
+            probabilities = F.softmax(logits[0], dim=-1)
+        
+        return probabilities
 
     def on_train_start(self):
         # by default lightning executes validation step sanity checks before training starts,
